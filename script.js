c471b0f7afa8381e07ce159a0cc9e67bf2f22eea
André Roberto Poleto de Lima <128004111+AndreLima44@users.noreply.github.com>
2024-09-10 22:02:03 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-  document.querySelectorAll('p[data-content]').forEach(function(p) {
-    p.addEventListener('click', function() {
-      var targetPage = this.getAttribute('data-content');
-      window.location.href = targetPage;
-    });
-  });
