e26b9239b2bee9fee4e9872a434472bdc203dcad
AlexandreFuruzawaScramin <alexandrescramin@gmail.com>
2024-10-07 21:56:50 -0300

    Formulario

--- X - TelaDeCadastrarAnuncio/js/script.js
+++ X - TelaDeCadastrarAnuncio/js/script.js
@@ -32,3 +32,54 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+
+
+let categoria = ''; 
+
+function selecionarCategoria(opcao) {
+    categoria = opcao;
+    document.getElementById('categoriaSelecionada').textContent = opcao;
+}
+
+
+
+document.getElementById('formCadastroAnuncio').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const dados = {
+        titulo: document.getElementById('titulo').value,
+        categoria: categoria, 
+        endereco: document.getElementById('endereco').value,
+        cidade: document.getElementById('cidade').value,
+        estado: document.getElementById('estado').value,
+        fotos: document.getElementById('fileInput').files[0],
+        quartos: document.getElementById('quarto').value,
+        banheiros: document.getElementById('banheiro').value,
+        garagem: document.getElementById('garagem').value,
+        area: document.getElementById('area').value,
+        descricao: document.getElementById('descricao').value
+    };
+
+    fetch('http://localhost:3000/cadastroAnuncio', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dados)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Erro na requisição: ' + response.status);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Sucesso:', data);
+        // Redireciona para a próxima página após o envio bem-sucedido
+        window.location.href = '../X - TelaFiltros/index.html';
+    })
+    .catch((error) => {
+        console.error('Erro:', error);
+        alert('Houve um problema ao enviar os dados.'); // Alerta para o usuário
+    })});
