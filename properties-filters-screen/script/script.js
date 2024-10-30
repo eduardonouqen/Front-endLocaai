0bbb2965af5ed79a103755bbe3ec2934900e8fb4
Andrelima44 <andrelimaben13@Outlook.com>
2024-10-29 22:08:31 -0300

    properties-filters-screen v0.1

---
+++ properties-filters-screen/script/script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+        const base64Url = token.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+
+        const userData = JSON.parse(jsonPayload);
+        const userName = userData.name;
+
+        document.querySelector('.textName').innerText = userName;
+    }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const newFiltersSidebar = document.getElementById('newFiltersSidebar');
+    const newFilterButton = document.getElementById('newFilterButton');
+
+    newFilterButton.addEventListener('click', () => {
+        newFiltersSidebar.classList.toggle('open'); // Adiciona ou remove a classe 'open'
+    });
+});
