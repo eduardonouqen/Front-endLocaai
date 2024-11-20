171a02f4b67ebfdc8ef165df6c14a123bd12217f
Eduardo de França Gonzalez <eduardogonzalez@alunos.unicesumar.edu.br>
2024-11-20 12:11:28 -0300

    top-bar correction

--- initial-screen/js/script.js
+++ initial-screen/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (token) {
         const base64Url = token.split('.')[1];
         const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
             return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
         }).join(''));
 
@@ -15,7 +15,17 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
+async function fetchUsers() {
+    const response = await fetch('http://localhost:3000/users'); // Replace with your API endpoint
+    const user = await response.json();
+    return user;
+}
 
+const token = localStorage.getItem("token");
+
+if (token) {
+    document.getElementById("statusLabel").textContent = "Locador Bronze";
+}
 
 
 
